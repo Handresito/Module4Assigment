19a8a2e89e7547f9630f7515743e0b082b4af12f
mauri <andres239@gmail.com>
2016-12-24 00:15:49 +0100

    corrupt files

--- src/routes.js
+++ src/routes.js
@@ -16,11 +16,23 @@
     })
     .state('categories',{
       url: '/categories',
-      templateUrl: 'src/restaurant/templates/categories.template.html'
+      templateUrl: 'src/restaurant/templates/categories.template.html',
+      controller: 'RestaurantController as controller',
+      resolve: {
+        categories: ['RestaurantService', function(RestaurantService){
+          return RestaurantService.getCategories();
+        }]
+      }
     })
     .state('items',{
-      url: '/items',
-      templateUrl: 'src/restaurant/templates/items.template.html'
+      url: '/items/{categoryName}',
+      templateUrl: 'src/restaurant/templates/items.template.html',
+      controller: 'ItemsController as itemCtrl',
+      resolve: {
+        category: ['RestaurantService', '$stateParams', function(RestaurantService, $stateParams){
+          return RestaurantService.getItemsForCategory($stateParams.categoryName);
+        }]
+      }
     })
   }
 })();
